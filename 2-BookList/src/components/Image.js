d6bac59f35476e655de568dfd1027bf74bc91102
WMouton <84017147+l33th@users.noreply.github.com>
2021-09-14 11:37:01 +0200

    Delete 2-BookList directory

--- 2-BookList/src/components/Image.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import BImage from '../images/book.jpeg';
-
-const Image = () => <img src={ BImage } />;
-
-export default Image;
--- 2-BookList/src/components/Title.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const title = 'The 7 Habits Of Highly Effective People';
-
-const Title = () => <h1>{ title }</h1>;
-
-export default Title
