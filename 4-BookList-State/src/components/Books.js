2cb44a0dbaa38e9327bf927508187a954beb903f
l33th <l33th@protonmail.ch>
2021-09-26 06:59:28 +0200

    Added More Complex onClick function

--- 4-BookList-State/src/components/Books.js
+++ 4-BookList-State/src/components/Books.js
@@ -1,20 +1,36 @@
 import React from "react";
 
 // Destructuring in the function parameter
-const Books = ({img, desc, title, author}) => {
+const Books = ({ img, desc, title, author }) => {
 	// Attribute, eventHandler
 	// onClick, onMouseOver
-	const clickHandler = () => {
-		alert('Hello, World!');
-	}
+	const clickHandler = (e) => {
+		console.log(e);
+		console.log(e.target);
+		alert("Hello, World!");
+	};
+
+	const complexExample = () => {
+		console.log(author);
+	};
 	return (
 		<div>
-			<article className='book'>
+			<article
+				className='book'
+				onMouseOver={() => {
+					console.log(title);
+				}}
+			>
 				<img src={img} alt='' />
 				<h5>{desc}</h5>
 				<h1>{title}</h1>
 				<h4>{author}</h4>
-				<button type='button' onClick={clickHandler}>reference examnle</button>
+				<button type='button' onClick={clickHandler}>
+					reference examnle
+				</button>
+				<button type='button' onClick={() => complexExample(author)}>
+					more complex example
+				</button>
 			</article>
 		</div>
 	);
